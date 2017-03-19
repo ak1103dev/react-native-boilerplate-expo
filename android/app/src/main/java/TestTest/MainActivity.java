5a4aad6b9881e3de4e11784a5c7b6a1efcae7c60
ak1103dev <ak.apichan1103@gmail.com>
2017-03-19 22:27:22 +0700

    :tada: eject android

---
+++ android/app/src/main/java/TestTest/MainActivity.java
@@ -0,0 +1,38 @@
+package TestTest;
+
+import com.facebook.react.ReactPackage;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import TestTest.generated.ExponentBuildConstants;
+import host.exp.expoview.ExponentActivity;
+
+public class MainActivity extends ExponentActivity {
+
+  @Override
+  public String publishedUrl() {
+    return "exp://exp.host/@ak1103dev/react-native-boilerplate2";
+  }
+
+  @Override
+  public String developmentUrl() {
+    return ExponentBuildConstants.DEVELOPMENT_URL;
+  }
+
+  @Override
+  public List<String> sdkVersions() {
+    return new ArrayList<>(Arrays.asList("15.0.0"));
+  }
+
+  @Override
+  public List<ReactPackage> reactPackages() {
+    return ((MainApplication) getApplication()).getPackages();
+  }
+
+  @Override
+  public boolean isDebug() {
+    return BuildConfig.DEBUG;
+  }
+}
